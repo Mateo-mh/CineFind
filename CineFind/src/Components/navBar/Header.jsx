8dbd1a14b5dde4095731f32a079a63ad0ae08cdf
Mateo-mh <byronmh8@gmail.com>
2024-06-24 16:52:31 -0500

    NavBar y responsive terminado

--- CineFind/src/Components/navBar/Header.jsx
+++ CineFind/src/Components/navBar/Header.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "./../../assets/images/logo.png";
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from './HeaderItem';
 
 function Header() {
+  const [toggle,setToggle]=useState(false);
   const menu = [
     { name: 'HOME', icon: HiHome },
     { name: 'SEARCH', icon: HiMagnifyingGlass },
@@ -20,10 +21,23 @@ function Header() {
     <header className="w-full fixed top-0 left-0 px-2 md:px-4 py-2 bg-[#1a202c] shadow-md z-10 flex items-center justify-between">
       <div className="flex items-center space-x-7">
         <img src={logo} alt="Logo" className="w-[35px] md:w-[40px] object-cover mr-4"/>
-        <div className="flex space-x-7">
+        <div className="hidden md:flex gap-8">
           {menu.map((item) => (
-            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+            <HeaderItem name={item.name} Icon={item.icon} />
           ))}
+        </div>
+        <div className="flex md:hidden gap-8">
+          {menu.map((item,index) => index<3&&(
+            <HeaderItem name={''} Icon={item.icon} />
+          ))}
+        <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={''} Icon={HiDotsVertical} />
+        {toggle? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+          {menu.map((item,index) => index>2&&(
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>:null}
+        </div>
         </div>
       </div>
       <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" alt="perfil"
